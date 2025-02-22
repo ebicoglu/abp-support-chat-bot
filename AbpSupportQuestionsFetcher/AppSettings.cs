fe53d6e2a07cb2d02bb7deb1da5b60f39cc615d6
Ebicoglu <alper.ebicoglu@volosoft.com>
2025-02-22 18:56:23 +0300

    refactor

---
+++ AbpSupportQuestionsFetcher/AppSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace AbpSupportQuestionsFetcher;
+
+public static class AppSettings
+{
+    //TODO UPDATE THESE OPTIONS
+    //------------------------------------------------------------------------------------------------
+    public static string OutputPdfPath = $"D:\\temp\\abp-support-{DateTime.Now:yyyy-MM-dd}.pdf";
+    public static string ConnectionString = "...Enter-Here...";
+    public static int? MaxRecordCount = null;
+    public static bool? OnlyAcceptedAnswers = true;
+    //------------------------------------------------------------------------------------------------
+
+}
